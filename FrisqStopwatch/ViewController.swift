5aac06482b9006e899f60f913a20ea5e2c82cdaf
Joakim Lundberg <joakim.lundb@gmail.com>
2019-11-09 12:10:23 +0100

    Initial Commit

---
+++ FrisqStopwatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FrisqStopwatch
+//
+//  Created by joakim lundberg on 2019-11-09.
+//  Copyright © 2019 joakim lundberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
